53555e6f3595eabc8abe241d5f1d2d67fb72b387
Evelina <evelinabalika@gmail.com>
2021-03-16 23:17:08 -0700

    Updated code to display both div and table ids

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -9,35 +9,49 @@ import { GetUrlService } from '../services/get-url.service';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-
   public url: string;
-  public output = new Array<string>();
-  public ids = new Array<string>();
+  public tableTags = new Array<string>();
+  public divTags = new Array<string>();
+  public tableIds = new Array<string>();
+  public divIds = new Array<string>();
 
-  constructor(private getURLService: GetUrlService) { 
+  constructor(private getURLService: GetUrlService) {
+
   }
 
   ngOnInit(): void {
+
   }
 
-  public submit()
-  {
-    this.ids = new Array<string>();
+  public submit() {
     var input = ((document.getElementById("input") as HTMLInputElement).value);
     this.url = input;
 
-    this.getURLService.getUrl(input).subscribe((result) =>{
-      this.output = result.div_tags || result.table_tags;
-      console.log(this.output);
-      
-      for(var i=0; i < this.output.length ; i++)
-      {
-        var parser = new DOMParser().parseFromString(this.output[i], "text/xml");
-        if (parser.children[0].id) 
-          this.ids.push(parser.children[0].id);
-        else if(parser.children[0].className)
-          this.ids.push(parser.children[0].className)
-      }
+    this.getURLService.getUrl(input).subscribe((result) => {
+      this.tableTags = result.table_tags;
+      this.divTags = result.div_tags;
+      this.getTableIds();
+      this.getDivIds();
     });
+  }
+
+  public getTableIds() {
+    for (var i = 0; i < this.tableTags.length; i++) {
+      var parser = new DOMParser().parseFromString(this.tableTags[i], "text/xml");
+      if (parser.children[0].id)
+        this.tableIds.push(parser.children[0].id);
+      else if (parser.children[0].className)
+        this.tableIds.push(parser.children[0].className)
+    }
+  }
+
+  public getDivIds() {
+    for (var i = 0; i < this.divTags.length; i++) {
+      var parser = new DOMParser().parseFromString(this.divTags[i], "text/xml");
+      if (parser.children[0].id)
+        this.divIds.push(parser.children[0].id);
+      else if (parser.children[0].className)
+        this.divIds.push(parser.children[0].className)
+    }
   }
 }
